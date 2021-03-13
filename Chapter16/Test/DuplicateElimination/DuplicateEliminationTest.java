520988a1eb8abf65e3aea4f4742504d7c669ec83
Peterbamidele <peterbamidele@gmail.com>
2021-03-14 00:17:28 +0100

    Test-Driven-Develpoment-With-Dietel-Exercise

---
+++ Chapter16/Test/DuplicateElimination/DuplicateEliminationTest.java
@@ -0,0 +1,22 @@
+package DuplicateElimination;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateEliminationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+    @Test
+    void checkName(){
+
+    }
+}
